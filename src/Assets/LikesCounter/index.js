d71fd214adcea3176d0d05049aba56309ed9444d
Vladyslav Kulyk <vladyslav.kulyk@gmail.com>
2019-10-21 14:23:45 +0300

    Lesson-2, task-2

--- src/Assets/LikesCounter/index.js
+++ src/Assets/LikesCounter/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import comment from './likes.png';
+import { useCounter } from './useCounter';
 
-export const LikesCounter = ({ counts }) => (
-  <>
-    <img src={comment} alt="likes" />
-    {counts}
-  </>
-);
+export const LikesCounter = ({ counts: countsInitial }) => {
+  const { counts, changeCount } = useCounter(countsInitial);
+
+  return (
+    <>
+      <button onClick={changeCount} type="button">
+        <img src={comment} alt="likes" />
+      </button>
+      {counts}
+    </>
+  );
+};
