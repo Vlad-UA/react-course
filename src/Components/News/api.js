382f5601cc95e174d5beafad9a2e393585ccdebe
Vladyslav Kulyk <vladyslav.kulyk@gmail.com>
2019-10-28 20:48:55 +0200

    Lesson 4, task 3. Added cache for fetching data news.

--- src/Components/News/api.js
+++ src/Components/News/api.js
@@ -2,8 +2,29 @@ const url = 'https://api.backendless.com/520C1D14-9413-E1AA-FF78-6A4DB0E26E00/9C
 
 export const api = {
   async getPosts() {
-    const response = await fetch(`${url}/data/news`);
+    const localStorageIds = {
+      postsDataLastRequestTime: 'postsDataLastRequestTime',
+      postsData: 'postsData',
+    };
 
-    return response.json();
+    const dateNowUnix = Math.floor(Date.now() / 1000);
+
+    const postsDataLastRequestTime = Number(
+      localStorage.getItem(localStorageIds.postsDataLastRequestTime),
+    );
+
+    const cacheRequestTimeMinutes = 10;
+
+    if ((dateNowUnix - postsDataLastRequestTime) / 60 > cacheRequestTimeMinutes) {
+      const response = await fetch(`${url}/data/news`);
+      const postsData = await response.json();
+
+      localStorage.setItem(localStorageIds.postsData, JSON.stringify(postsData));
+      localStorage.setItem(localStorageIds.postsDataLastRequestTime, dateNowUnix.toString());
+
+      return postsData;
+    }
+
+    return JSON.parse(localStorage.getItem(localStorageIds.postsData));
   },
 };
